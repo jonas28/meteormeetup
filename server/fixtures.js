ba4a8e717d07af649a3b7e2740054252ceefde33
jonas28 <jonas@steps.fm>
2015-03-10 14:54:00 +0100

    added post detail template, changed fixtures.js to fb insights

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,15 @@
 if (Posts.find().count() === 0) {
     Posts.insert({
-        title: 'Google',
-        url: 'http://google.de'
+        from_id: '133615754492',
+        object_id: '10153015197004493',
+        type: 'photo',
+        picture: 'https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/p130x130/1459321_10152977884604493_2590889144617360481_n.png?oh=4533c1f448a6b71abb74ef6a3bb359a3&oe=558F4589&__gda__=1433655628_4be496298c01cfd82a744380ae4ade6e',
+        likes: '46278',
+        comments: '',
+        shares: '4487',
+        post_impressions: '2570246',
+        photo_views: '51379',
+        link_clicks: '4'
     });
 
-    Posts.insert({
-        title: 'Facebook',
-        url: 'http://facebook.com'
-    });
-
-    Posts.insert({
-        title: 'YouTube',
-        url: 'http://youtube.com'
-    });
 }
